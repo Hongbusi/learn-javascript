150a92c292e27bfbdd036727ff6d556f9ffad35e
Hongbusi <q951686662@qq.com>
2021-09-28 18:29:49 +0800

    feat: init

--- src/core/instance/index.js
+++ src/core/instance/index.js
@@ -5,15 +5,19 @@ import { eventsMixin } from './events'
 import { lifecycleMixin } from './lifecycle'
 import { warn } from '../util/index'
 
+// Vue 构造函数
 function Vue (options) {
   if (process.env.NODE_ENV !== 'production' &&
     !(this instanceof Vue)
   ) {
     warn('Vue is a constructor and should be called with the `new` keyword')
   }
+
+  // 调用 Vue.prototype._init 方法，该方法是在 initMixin 中定义的
   this._init(options)
 }
 
+// 定义 Vue.prototype._init 方法
 initMixin(Vue)
 stateMixin(Vue)
 eventsMixin(Vue)
