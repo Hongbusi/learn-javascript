9a44389cf72a69face9daab9941f19434d45f55a
Hongbusi <q951686662@qq.com>
2022-03-03 22:56:39 +0800

    feat: Array.prototype.at

--- Array.prototype.at.js
+++ Array.prototype.at.js
@@ -6,12 +6,12 @@ function at(n) {
 }
 
 // test
-Array.prototype.hAt = at
+Array.prototype._at = at
 
 const arr = [0, 1, 2, 3, 4, 5, 6]
 
-console.log(arr.hAt(0))
-console.log(arr.hAt(6))
-console.log(arr.hAt(9))
-console.log(arr.hAt(-1))
-console.log(arr.hAt(-9))
+console.log(arr._at(0))
+console.log(arr._at(6))
+console.log(arr._at(9))
+console.log(arr._at(-1))
+console.log(arr._at(-9))
--- Array.prototype.concat.js
+++ Array.prototype.concat.js
@@ -2,4 +2,8 @@ function concat() {
 
 }
 
-Array.prototype.hycConcat = concat
+Array.prototype._concat = concat
+
+const arr1 = [0, 1, 2, 3]
+const arr2 = [3, 4, 5, 6]
+
